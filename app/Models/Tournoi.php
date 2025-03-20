1bfce85833b978cefd7e6a3ef82872f1c22b2236
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-20 22:36:44 +0000

    Add Tournoi model with relationships to Matche and User models

---
+++ app/Models/Tournoi.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tournoi extends Model
+{
+    protected $fillable = [
+        'tournoi_id',
+        'match_date',
+        'rules',
+        'nb_players',
+        'image',
+    ];
+
+    public function mathe(){
+        return $this->hasMany(Matche::class);
+    }
+
+    public function User()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
