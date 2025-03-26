08b7f136bb21ede084d7d17a3daa24b25290f2f8
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-26 22:44:51 +0000

    update TournoiController

--- app/Http/Controllers/TournoiController.php
+++ app/Http/Controllers/TournoiController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tournoi;
+use App\Models\User;
 
 class TournoiController extends Controller
 {
     public function AddTournoi(Request $request){
         $validatorData = $request->validate([
+            'name' => 'required|string|max:255',
             'rules' => 'required|string|max:255', 
             'nb_players' => 'required|integer|min:1', 
             'image' => 'nullable|image|max:2048', 
         ]);
+        $validatorData['creator_id'] = auth()->user()->id;
 
         try{
             $tournoi = Tournoi::create($validatorData);
@@ -55,12 +58,16 @@ class TournoiController extends Controller
             'rules' => 'string|max:255', 
             'nb_players' => 'integer|min:1', 
             'image' => 'nullable|image|max:2048', 
+            'name' => 'nullable|string|max:255',
         ]);
 
         
 
         try{
             $tournoi = Tournoi::findOrFail($id);
+            if ($tournoi->creator_id != auth()->user()->id) {
+                return response()->json(['message' => 'Unauthorized', 'error' => 'You are not the creator of this tournoi'], 403);
+            }
             $tournoi->update($validatorData);
             return response()->json(['message'=> 'Tournoi updated successfully','tournoi'=>$tournoi],200);
         }catch(\Exception $e){
@@ -73,6 +80,9 @@ class TournoiController extends Controller
     public function delete($id){
         try{
             $tournoi = tournoi::findOrFail($id);
+            if ($tournoi->creator_id != auth()->user()->id) {
+                return response()->json(['message' => 'Unauthorized', 'error' => 'You are not the creator of this tournoi'], 403);
+            }
             $tournoi->delete();
             return response()->json(['message'=>'tournoi deleted successfully'],200);
         }catch(\Exception $e){
