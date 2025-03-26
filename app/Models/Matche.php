674b31d34b34f12629954cd5be42e63e4889a1c1
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-26 22:44:07 +0000

    add HasFactory trait to models

--- app/Models/Matche.php
+++ app/Models/Matche.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Matche extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'tournoi_id',
         'player_1_id',
--- app/Models/Tournoi.php
+++ app/Models/Tournoi.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tournoi extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'tournoi_id',
+        'name',
         'rules',
         'nb_players',
         'image',
+        'creator_id'
     ];
 
     public function mathe(){
@@ -20,5 +24,10 @@ class Tournoi extends Model
     public function User()
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
     }
 }
