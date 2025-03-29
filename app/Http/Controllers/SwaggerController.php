b3e141396e953022b61bd95cae68053dbe367203
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-29 22:24:13 +0000

    Add SwaggerController for API documentation

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *      title="Your API Title",
+ *      version="1.0.0",
+ *      description="API Documentation for Your Application",
+ *      @OA\Contact(
+ *          email="contact@yourapp.com"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url="http://localhost:8000",
+ *      description="Local Development Server"
+ * )
+ */
+class SwaggerController extends Controller
+{
+    //
+}
